c6192f103dcb928576eca6f72dd91dd10878d97c
KarenViolim <karen.violim2011@gmail.com>
2019-05-31 20:38:35 -0300

    Coloquei teste de par ou impar

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void checar_idade_par_impar(int i)
+{
+	if (i & 0x01)
+		printf("eh impar\n");
+	else
+		printf("eh par\n");
+}
+
 int main()
 {
     int idade, *p;
@@ -10,10 +18,12 @@ int main()
     *p = 22;
 
     printf("Ola mundo, a idade é %i\n", idade);
+    checar_idade_par_impar(idade);
 
     idade = 31;
     
     printf("Ola mundo, a idade é %i\n", *p);
+    checar_idade_par_impar(idade);
 
     printf("O endereço da variável idade é: %p\n",p);    
 
